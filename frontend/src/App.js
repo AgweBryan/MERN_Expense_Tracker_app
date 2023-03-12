5b2556f3b4f5f8617db680b159f07b325c55e029
AgweBryan <agwebryanmunakwa@gmail.com>
2023-03-13 00:23:40 +0100

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-6xl underline uppercase font-bold absolute -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2">
+        Welcome Back <span className="text-green-500">Bryan</span>
+      </h1>
+    </div>
+  );
+}
+
+export default App;

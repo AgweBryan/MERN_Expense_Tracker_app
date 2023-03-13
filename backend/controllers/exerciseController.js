c38d2eb74f0af95d0c77337eb5e5809127eb96e9
AgweBryan <agwebryanmunakwa@gmail.com>
2023-03-13 02:01:58 +0100

    add CRUD ops for exercises

--- backend/controllers/exerciseController.js
+++ backend/controllers/exerciseController.js
@@ -1,3 +1,4 @@
+const mongoose = require("mongoose");
 const Exercise = require("../models/exerciseModel");
 
 const getAllExercises = async (req, res) => {
@@ -24,4 +25,43 @@ const postAddExercise = async (req, res) => {
   }
 };
 
-module.exports = { getAllExercises, postAddExercise };
+const patchUpdateExcersise = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "No such excercise" });
+  }
+
+  const excercise = await Exercise.findOneAndUpdate(
+    { _id: id },
+    { ...req.body }
+  );
+
+  if (!excercise) {
+    return res.status(400).json({ error: "No such excercise" });
+  }
+
+  res.status(200).json(excercise);
+};
+
+const deleteExercise = async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "No such exercise" });
+  }
+
+  const exercise = await Exercise.findOneAndDelete({ _id: id });
+
+  if (!exercise) {
+    return res.status(400).json({ error: "No such exercise" });
+  }
+
+  res.status(200).json(exercise);
+};
+
+module.exports = {
+  getAllExercises,
+  postAddExercise,
+  patchUpdateExcersise,
+  deleteExercise,
+};
--- backend/routes/exercise.js
+++ backend/routes/exercise.js
@@ -2,11 +2,18 @@ const express = require("express");
 const {
   getAllExercises,
   postAddExercise,
+  patchUpdateExcersise,
+  deleteExercise,
 } = require("../controllers/exerciseController");
 
 const router = express.Router();
 
 router.get("/", getAllExercises);
+
 router.post("/add", postAddExercise);
+
+router.patch("/update/:id", patchUpdateExcersise);
+
+router.delete("/delete/:id", deleteExercise);
 
 module.exports = router;
